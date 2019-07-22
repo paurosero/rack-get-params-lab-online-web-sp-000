dd9127f70cbfd08524caeec28c0ae7107444f023
Paul Rosero <paul_rosero@yahoo.com>
2019-07-22 00:34:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,15 +11,20 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+      
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
         @@cart.each {|item| resp.write "#{item}\n"
       end
+      
+    elsif req.path.match(/add/)
+    
     else
       resp.write "Path Not Found"
     end
