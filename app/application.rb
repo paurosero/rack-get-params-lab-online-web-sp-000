3043aeab0d6ac5f8c768460ed41dc09fb38f1a41
Paul Rosero <paul_rosero@yahoo.com>
2019-07-22 00:31:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,10 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        
+      end
     else
       resp.write "Path Not Found"
     end
